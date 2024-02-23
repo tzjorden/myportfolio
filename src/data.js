7b3d7ce4757096513789e937576f7de054a34d33
Tenzin Jorden <tenden96@gmail.com>
2024-02-23 14:36:16 -0500

    uploading project

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+      title: "TibetTV",
+      subtitle: "Laravel/Php/Blade",
+      description:
+        "Created a database for TibetTV to store all their videos. Built roles for admins/users to specify the roles of each user.",
+      image: "./tibettv.gif",
+    },
+    {
+      title: "Political Prisoner",
+      subtitle: "Laravel/Php/Livewire",
+      description:
+        "Built an application to store the data of all the innocent civilians in Tibet who have been imprisoned for protesting against tyranny.",
+      image: "./political.gif",
+    },
+  ];
+  
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Ruby on Rails",
+    "Node",
+    "Php",
+    "Laravel",
+    "Tailwind",
+    "MySQL",
+    "PostgreSQL",
+    "Livewire"
+  ];
